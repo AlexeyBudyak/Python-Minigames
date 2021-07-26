4bbfba47e2d09afe947a01d3f7999e52523def17
Alexey Budyak <70070533+AlexeyBudyak@users.noreply.github.com>
2021-07-26 00:54:31 -0700

    Update and rename heads-and-legs.py to heads-or-tails.py

--- heads-or-tails.py
+++ heads-or-tails.py
@@ -15,12 +15,3 @@ if player == coin:
     print("you won yay!!!!!!!")
 else:
     print ('you lost and i won hahahahaha')
-rate = input('How was the game? good,bad normal? ') 
-if rate == 'good':
-    print('Yay!')
-elif rate == "bad":
-    print(':-(')
-elif rate == 'normal':
-  print ('okay')
-else:
-    print("I don't understand you")
